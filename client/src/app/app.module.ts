c43fc1f8092e446760fb68dbfbb25d56305a3f0f
Carlos Andrés Martinez Cordoba - Ceiba Software <carlos.martinez@ceiba.com.co>
2018-08-14 16:12:31 -0500

    organiza el archivo gitignore

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { VehiculosComponent } from './vehiculos/vehiculos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    VehiculosComponent
   ],
   imports: [
     BrowserModule
