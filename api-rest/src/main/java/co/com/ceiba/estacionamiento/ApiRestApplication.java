42d2770dbe148d79d209797675c9964845dea902
Carlos Andrés Martinez Cordoba - Ceiba Software <carlos.martinez@ceiba.com.co>
2018-08-27 09:18:33 -0500

    test6

--- api-rest/src/main/java/co/com/ceiba/estacionamiento/ApiRestApplication.java
+++ api-rest/src/main/java/co/com/ceiba/estacionamiento/ApiRestApplication.java
@@ -1,4 +1,4 @@
-package com.parking;
+package co.com.ceiba.estacionamiento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
